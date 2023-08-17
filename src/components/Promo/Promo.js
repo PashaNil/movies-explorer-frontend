e3e68b3c5d096d3da77918a3b4c8bfdf9927ca14
Pavel Danilov <Sony1000@yandex.ru>
2023-08-17 04:02:28 +0300

    fixing section promo

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -5,7 +5,7 @@ function Promo() {
   return (
     <section className="promo">
       <img className="promo__img" src={webEarth} alt="Изображение планеты из слов web" />
-      <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+      <h1 className="promo__title">Учебный проект студента факультета Веб&#8209;разработки.</h1>
       <p className="promo__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
       <button
         className="promo__scroll"
